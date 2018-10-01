3fef83461ef112ea0c0decd1b5a5b53ee8b21934
Lorenzo Sinisi <lasslo.net@gmail.com>
2018-10-01 14:33:38 +0200

    Add the genserver module to the mix project

--- lib/phoenix_postgres_pub_sub.ex
+++ lib/phoenix_postgres_pub_sub.ex
@@ -1,18 +1,39 @@
 defmodule PhoenixPostgresPubSub do
-  @moduledoc """
-  Documentation for PhoenixPostgresPubSub.
-  """
+  use GenServer
+
+  require Logger
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PhoenixPostgresPubSub.hello
-      :world
-
+  Initialize the GenServer
   """
-  def hello do
-    :world
+  @spec start_link(List.t()) :: {:ok, pid}
+  def start_link(channels), do: GenServer.start_link(__MODULE__, channels, name: __MODULE__)
+
+  @doc """
+  When the GenServer starts subscribe to the given channel
+  """
+  @spec init(List.t()) :: {:ok, []}
+  def init(channels) do
+    pg_config = DataBucket.Repo.config()
+    {:ok, pid} = Postgrex.Notifications.start_link(pg_config)
+
+    list_of_channels = List.wrap(channels)
+    Enum.map(list_of_channels, fn channel -> Postgrex.Notifications.listen(pid, channel) end)
+
+    {:ok, {pid, channels, nil}}
+  end
+
+  @doc """
+  Listen for changes
+  """
+  def handle_info(notification, _state) do
+    adapter = adapter_from_config()
+    apply(adapter, :handle_postgres_notification, [notification])
+
+    {:noreply, :event_handled}
+  end
+
+  defp adapter_from_config() do
+    Application.fetch_env!(:phoenix_postgres_pubsub, :adapter)
   end
 end
